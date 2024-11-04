b5236a65375419851c8338e6ed22b5050a233e49
Rina Lisa <anpast2014@yandex.ru>
2024-11-04 10:11:30 +0300

    fix cypress constants and request check

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
       // implement node event listeners here
     },
   },
+  env: {
+    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MTZiMmE2ZDgyOWJlMDAxYzc3Nzg5OCIsImlhdCI6MTczMDI4MDU1NSwiZXhwIjoxNzMwMjgxNzU1fQ.pTD__u6BJdkRr-bz35lXr6VcFqPspLACAjGdEIBF1xs',
+    refreshToken: 'e24fe3f2b3d2a401eee510a6206bb9e7831ffbf65fbbb79b934b4b13e4d5c59dcf02dce0662212c6',
+  }
 });
--- cypress/e2e/constructor/burger-constructor.cy.ts
+++ cypress/e2e/constructor/burger-constructor.cy.ts
@@ -1,14 +1,50 @@
 import { deleteCookie, setCookie } from '../../../src/utils/cookie';
 
+const testId = {
+    bun: {
+        id: '643d69a5c3f7b9001cfa093d',
+        name: 'Флюоресцентная булка R2-D3'
+    },
+    ingredients: [
+        {
+            id: '643d69a5c3f7b9001cfa0943',
+            name: 'Соус фирменный Space Sauce'
+        },
+        {
+            id: '643d69a5c3f7b9001cfa0946',
+            name: 'Хрустящие минеральные кольца'
+        },
+    ]
+}
+
+const testOrder = {
+    bun: {
+        id: '643d69a5c3f7b9001cfa093d',
+        name: 'Флюоресцентная булка R2-D3'
+    },
+    ingredients: [
+        {
+            id: '643d69a5c3f7b9001cfa0949',
+            name: 'Мини-салат Экзо-Плантаго'
+        },
+        {
+            id: '643d69a5c3f7b9001cfa0942',
+            name: 'Соус Spicy-X'
+        },
+        {
+            id: '643d69a5c3f7b9001cfa0941',
+            name: 'Биокотлета из марсианской Магнолии'
+        },
+    ]
+}
 
 describe('тест для страницы конструктора бургера', function() {
     beforeEach(() => {
-        setCookie('accessToken', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3MTZiMmE2ZDgyOWJlMDAxYzc3Nzg5OCIsImlhdCI6MTczMDI4MDU1NSwiZXhwIjoxNzMwMjgxNzU1fQ.pTD__u6BJdkRr-bz35lXr6VcFqPspLACAjGdEIBF1xs');
-        localStorage.setItem('refreshToken', 'e24fe3f2b3d2a401eee510a6206bb9e7831ffbf65fbbb79b934b4b13e4d5c59dcf02dce0662212c6');
+        setCookie('accessToken', `Bearer ${Cypress.env('accessToken')}`);
+        localStorage.setItem('refreshToken', Cypress.env('refreshToken'));
 
         cy.intercept('GET', 'api/ingredients', {fixture: 'ingredients'}).as('getIngredients');
         cy.intercept('GET', 'api/auth/user', {fixture: 'user'}).as('getUser');
-        cy.intercept('POST', 'api/orders', {fixture: 'order'}).as('addOrder');
 
         cy.visit('');
         
@@ -21,28 +57,28 @@ describe('тест для страницы конструктора бургер
     it('Тест подгрузки ингредиентов и добавления их в конструктор', () => {
         cy.get('[data-cy="constructor"]').as('constructor');
         
-        cy.addIngredient('643d69a5c3f7b9001cfa093d');
-        cy.addIngredient('643d69a5c3f7b9001cfa0943');
-        cy.addIngredient('643d69a5c3f7b9001cfa0946');
+        cy.addIngredient(testId.bun.id);
+        cy.addIngredient(testId.ingredients[0].id);
+        cy.addIngredient(testId.ingredients[1].id);
         
-        cy.get('@constructor').should('contain', 'Флюоресцентная булка R2-D3');
-        cy.get('@constructor').should('contain', 'Соус фирменный Space Sauce');
-        cy.get('@constructor').should('contain', 'Хрустящие минеральные кольца');
+        cy.get('@constructor').should('contain', testId.bun.name);
+        cy.get('@constructor').should('contain', testId.ingredients[0].name);
+        cy.get('@constructor').should('contain', testId.ingredients[1].name);
         
     })
     describe('Тест правильной работы модального окна', function() {
         it('Открытие модального окна ингредиента', () => {
             
             cy.get('[data-cy="modal"]').should('not.exist')
-            cy.get('[data-cy="643d69a5c3f7b9001cfa093d"]').first().click();
+            cy.get(`[data-cy="${testId.bun.id}"]`).first().click();
 
             cy.get('[data-cy="modal"]').as('modal');
             cy.get('@modal').should('exist');
-            cy.get('@modal').should('contain', 'Флюоресцентная булка R2-D3');
+            cy.get('@modal').should('contain', testId.bun.name);
             
         })
         it('Закрытие модального окна по крестику', () => {
-            cy.get('[data-cy="643d69a5c3f7b9001cfa093d"]').first().click();
+            cy.get(`[data-cy="${testId.bun.id}"]`).first().click();
 
             cy.get('[data-cy="modal"]').as('modal');
             cy.get('@modal').should('exist');
@@ -52,7 +88,7 @@ describe('тест для страницы конструктора бургер
             
         })
         it('Закрытие модального окна по клику на оверлей', () => {
-            cy.get('[data-cy="643d69a5c3f7b9001cfa093d"]').first().click();
+            cy.get(`[data-cy="${testId.bun.id}"]`).first().click();
 
             cy.get('[data-cy="modal"]').as('modal');
             cy.get('@modal').should('exist');
@@ -62,7 +98,7 @@ describe('тест для страницы конструктора бургер
             
         })
         it('Закрытие модального окна по клику на Esc', () => {
-            cy.get('[data-cy="643d69a5c3f7b9001cfa093d"]').first().click();
+            cy.get(`[data-cy="${testId.bun.id}"]`).first().click();
 
             cy.get('[data-cy="modal"]').as('modal');
             cy.get('@modal').should('exist');
@@ -79,26 +115,36 @@ describe('тест для страницы конструктора бургер
             cy.get('[data-cy="order_button"]').as('addOrderButton');
             cy.get('@addOrderButton').should('be.disabled');
         
-            cy.addIngredient('643d69a5c3f7b9001cfa093d');
+            cy.addIngredient(testOrder.bun.id);
             cy.get('@addOrderButton').should('be.disabled');
-            cy.addIngredient('643d69a5c3f7b9001cfa0946');
+            cy.addIngredient(testOrder.ingredients[0].id);
             cy.get('@addOrderButton').should('be.enabled');       
         })
         it('Проверка оформления заказа', () => {
             cy.get('[data-cy="constructor"]').as('constructor');
         
-            cy.addIngredient('643d69a5c3f7b9001cfa093d');
-            cy.addIngredient('643d69a5c3f7b9001cfa0949');
-            cy.addIngredient('643d69a5c3f7b9001cfa0942');
-            cy.addIngredient('643d69a5c3f7b9001cfa0941'); 
+            cy.addIngredient(testOrder.bun.id);
+            cy.addIngredient(testOrder.ingredients[0].id);
+            cy.addIngredient(testOrder.ingredients[1].id);
+            cy.addIngredient(testOrder.ingredients[2].id); 
             
-            cy.get('@constructor').should('contain', 'Флюоресцентная булка R2-D3');
-            cy.get('@constructor').should('contain', 'Соус Spicy-X');
-            cy.get('@constructor').should('contain', 'Мини-салат Экзо-Плантаго');
-            cy.get('@constructor').should('contain', 'Биокотлета из марсианской Магнолии');
+            cy.get('@constructor').should('contain', testOrder.bun.name);
+            cy.get('@constructor').should('contain', testOrder.ingredients[0].name);
+            cy.get('@constructor').should('contain', testOrder.ingredients[1].name);
+            cy.get('@constructor').should('contain', testOrder.ingredients[2].name);
 
             cy.get('[data-cy="order_button"]').as('addOrderButton');
             cy.get('@addOrderButton').click();
+            cy.intercept('POST', 'api/orders', {fixture: 'order'}).as('addOrder');
+            cy.wait('@addOrder').then(({request}) => {
+                expect(request.headers.authorization).to.eq(`Bearer ${Cypress.env('accessToken')}`);
+                expect(request.body.ingredients).to.have.length(5);
+                expect(request.body.ingredients[0]).to.eq(testOrder.bun.id);
+                expect(request.body.ingredients[1]).to.eq(testOrder.ingredients[0].id)
+                expect(request.body.ingredients[2]).to.eq(testOrder.ingredients[1].id)
+                expect(request.body.ingredients[3]).to.eq(testOrder.ingredients[2].id)
+                expect(request.body.ingredients[4]).to.eq(testOrder.bun.id)
+              })
 
             cy.get('[data-cy="modal"]').as('modal');
             cy.get('@modal').should('exist');
@@ -107,10 +153,10 @@ describe('тест для страницы конструктора бургер
             cy.get('[data-cy="modal_close"]').click();
             cy.get('@modal').should('not.exist');
 
-            cy.get('@constructor').should('not.contain', 'Флюоресцентная булка R2-D3');
-            cy.get('@constructor').should('not.contain', 'Соус Spicy-X');
-            cy.get('@constructor').should('not.contain', 'Мини-салат Экзо-Плантаго');
-            cy.get('@constructor').should('not.contain', 'Биокотлета из марсианской Магнолии');
+            cy.get('@constructor').should('not.contain', testOrder.bun.name);
+            cy.get('@constructor').should('not.contain', testOrder.ingredients[0].name);
+            cy.get('@constructor').should('not.contain', testOrder.ingredients[1].name);
+            cy.get('@constructor').should('not.contain', testOrder.ingredients[2].name);
 
             cy.get('[data-cy="order_button"]').as('addOrderButton');
             cy.get('@addOrderButton').should('be.disabled');
